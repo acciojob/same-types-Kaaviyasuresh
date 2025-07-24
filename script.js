bb7bc2ae1dd8947cc3fafbf04a71b88a6692b91f
kaaviyashree s <86909004+Kaaviyasuresh@users.noreply.github.com>
2025-07-24 13:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ function isSameType(value1, value2) {
 }
 
 
-// do not change the code below.
-let value1 = ("Enter Start of the Range.");
-let value2 = ("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+let value1 = prompt("Enter Start of the Range.");
+let value2 = prompt("Enter End Of the Range.");
+
+// Try converting to numbers
+const num1 = Number(value1);
+const num2 = Number(value2);
+
+alert(isSameType(num1, num2));
+
