5d3793e93aa51a82643c0ea688ba458707036ae5
kaaviyashree s <86909004+Kaaviyasuresh@users.noreply.github.com>
2025-07-24 13:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-function isSameType(value1, value2) {
-  if (Number.isNaN(value1) || Number.isNaN(value2)) {
-    return true;
-  } else if (typeof value1 === typeof value2) {
-    return true;
-  } else {
+function isSameType(value1, value2) if (Number.isNaN(value1) || Number.isNaN(value2)) {
     return false;
   }
+  return typeof value1 === typeof value2;
 }
 
 // do not change the code below.
